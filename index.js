1e64ff6086dada675261c408188be0b19a17e8c9
JordiSM <61203146+JordiSM@users.noreply.github.com>
2022-04-10 17:54:12 +0200

    Version 1.0
    Fixed bug with recursive function (made the function iterative), flags functionality, added some style, Victory & Game Over text and button to start a newGame

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.getElementById('grid')
+    grid = document.getElementById('grid')
     const width = 10
     const nBombs = 20
     
     let squares = []
+    let bombs = []
 
     let SquaresLeft
     let isGameOver
-    let flags
+    let flagsLeft = nBombs
 
     function createBoard() {
         SquaresLeft = (width * width) - nBombs
         isGameOver  = false
-        flags = 0
+        flagsLeft = 0
 
         for (let i = 0; i < width; i++) {
             squares[i] = []
@@ -21,8 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 squares[i][j] = 0
             }
         }
-        
-        bombs_counter = 0;
+        bombs = []
+        bombs_counter = 0
         while(bombs_counter < nBombs){
            
             x = Math.floor(Math.random() * (width - 0));
@@ -32,6 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
             if(squares[x][y] != -1){
                 //colocamos la bomba
                 squares[x][y] = -1
+                bombs.push((x * width) + y)
 
                 //actualizamos casillas vecinas
                 for(let ix = -1; ix < 2; ix++){
@@ -51,6 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for(let i = 0; i < width*width; i++ ){
             const square = document.createElement('div')
             square.setAttribute('id', i)
+            console.log(square)
             data = squares[Math.floor(i/width)][i%width]
             if(data!= -1){
                 square.classList.add('valid')
@@ -73,56 +76,101 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(squares)
     }
 
+    function gameOver(square){
+        message = document.getElementById('message')
+        message.innerHTML = 'Game Over'
+        isGameOver=true
+        bombs.forEach(id => {
+            bomb = document.getElementById(id)
+            bomb.innerHTML = 'x'
+        });
+    }
 
-    function click(square) {
-        currentId = square.id
-        x = Math.floor(currentId/10)
-        y = currentId%10
-        console.log('Fila:' ,x, ' - Columna:' , y)
-        
+    function Victory(){
+        message = document.getElementById('message')
+        message.innerHTML = 'Victory!!'
+        isGameOver=true
+    }
+
+    function click(square){
         if(isGameOver) return
         if(square.classList.contains('checked') || square.classList.contains('flag')) return
         if(square.classList.contains('bomb')){
             gameOver(square)
-        } else {
-
-            value = square.getAttribute('data')
-            console.log(value)
-
-            square.classList.add('checked')
-
-            if(value == 0){
-                checkNeighbours(square, currentId)
-            }
+            return
         }
-    }
 
-    function gameOver(square){
-        console.log('bomba en ', square.id)
-    }
-
-    function checkNeighbours(square , currentId){
+        currentId = square.id
         x = Math.floor(currentId/10)
         y = currentId%10
-        setTimeout(() => {
-            for(let ix = -1; ix < 2; ix++){
-                for(let iy = -1; iy < 2; iy++){
-                    posx = x + ix;
-                    posy = y + iy;
-                    if((-1 < posx) && (posx < width) && (-1 < posy) && (posy < width)){
-                        nextId = posx * 10 + posy
-                        click(document.getElementById(nextId))
+
+        cells = []
+        cells.push(currentId)
+        let cell_id
+
+        while(cells.length > 0){
+            cell_id = cells.shift()
+            cell = document.getElementById(cell_id)
+            if(cell.classList.contains('checked')){
+                continue
+            }
+            value = cell.getAttribute('data')
+            cell.innerHTML = value
+            cell.classList.add('checked')
+            SquaresLeft--
+            if(SquaresLeft == 0){
+                Victory()
+            }
+            if(parseInt(value) == 0){
+                cell.innerHTML = ''
+               
+                x = Math.floor(cell_id/10)
+                y = cell_id%10
+                
+                for(let ix = -1; ix < 2; ix++){
+                    for(let iy = -1; iy < 2; iy++){
+                        if (ix === 0 && iy === 0) {
+                            continue;
+                        }
+                        posx = x + ix;
+                        posy = y + iy;
+                        if((-1 < posx) && (posx < width) && (-1 < posy) && (posy < width)){
+                            nextId = (posx * 10) + posy
+                            cells.push(nextId)
+                        }
                     }
                 }
             }
-        }, 50)
+
+
+        }
     }
-
-
 
     function addFlag(square){
-        console.log('flag')
+        if(isGameOver) return
+        if(square.classList.contains('checked')) return
+        if(flagsLeft==0) return
+        if(square.classList.contains('flag')){
+            square.classList.remove('flag')
+            square.innerHTML = ''
+            flagsLeft++
+        } else {
+            square.classList.add('flag')
+            square.innerHTML = '!'
+            flagsLeft--
+        }
     }
+
+    face = document.getElementById('newGame')
+    face.addEventListener('click' , () => {
+        //deletes previous board
+        document.getElementById('message').innerHTML = ''
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild);
+        }
+        //create board
+        createBoard()
+    })
 
     createBoard()
 
